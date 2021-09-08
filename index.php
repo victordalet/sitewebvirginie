b6e0040cc2d0272c9217ab8a8f726845660c487d
victordalet <victordalet@protonmail.com>
2021-09-08 19:31:08 +0200

    debt page principale + css attente text et image

---
+++ index.php
@@ -0,0 +1,36 @@
+<!doctype HTML>
+<html lang="fr">
+      <head> 
+	      <title> VIRGINIE DUHAMEL  </title> <!--le titre de la page -->
+		<link rel="stylesheet" type="text/css" href="css/style_div.css"> <!-- on imparte le css -->
+		<link rel="stylesheet" type="text/css" href="css/style_police.css"> <!-- on imparte le css -->
+		<link rel="stylesheet" type="text/css" href="css/style_bouton.css"> <!-- on imparte le css -->
+	      <link rel="icon" type="image/png" href="images/logo.png" /> <!-- logo à coté du titre-->
+	</head>
+	        
+	<body>
+		<div id="bandeau">
+			<h1 id= "logo"> 
+				VIRGINIE DUHAMEL 
+				<span id="first_element" class="choix_pages">
+					Virginie
+				</span> 
+				<span class="choix_pages">
+					Collage
+				</span> 
+				<span class="choix_pages">
+					Peinture
+				</span> 
+				<span class="choix_pages">
+					Contacte
+				</span> 				
+			</h1>
+		</div>
+		<div id="contenu_pres">
+			Ceci est le contenu
+		</div>
+		<div id="piedpage">
+			Ceci est le pied de page
+		</div>
+      </body>
+</html>
