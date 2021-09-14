d900b6c6353ee891295a4bc20e5af48810ed2210
victordalet <victordalet@protonmail.com>
2021-09-14 21:59:01 +0200

    page 404

--- 404.php
+++ 404.php
@@ -1,24 +1,19 @@
 <!doctype HTML>
 <html lang="fr">
       <head> 
-	       <title> nsi </title> <!--le titre de la page -->
-		   <link rel="stylesheet" href="css/404.css"> <!-- on imparte le css -->
-		   <script src="js/404.js"></script> <!-- on import le js -->
-		   <script src="js/4042.js"></script> <!-- on import le js -->
-		   <link rel="icon" type="image/png" href="images/logo.png" /> <!-- logo à coté du titre-->
+		<title> 404 VIRGINIE DUHAMEL  </title> <!--le titre de la page -->
+		<link rel="stylesheet" type="text/css" href="css/style_div.css"> <!-- on imparte le css -->
+		<link rel="stylesheet" type="text/css" href="css/style_image.css"> <!-- on imparte le css -->
+		<link rel="stylesheet" type="text/css" href="css/style_police.css"> <!-- on imparte le css -->
+		<link rel="stylesheet" type="text/css" href="css/style_bouton.css"> <!-- on imparte le css -->
+		<script src="js/galerie.js"></script> <!-- on import le js -->
+		<script src="js/redirection.js"></script> <!-- on import le js -->
+	      <link rel="icon" type="image/png" href="images/logo.png" /> <!-- logo à coté du titre-->
 	  </head>
-	  <body onkeydown="x,y,point = Touche(event,x,y,lancer,point);"> 
+	  <body> 
 	  	    <h1>Erreur 404 cette page n'existe pas! </h1>
-	  	    <div id="logo404"><input type="button" value="Revenir à la page d'accueil" onclick="redirection();"> <br>
+	  	    <div id="logo404"><input type="button" value="Revenir à la page d'accueil" onclick="redirect(0,1);"> <br>
 	  	    <img alt="404" src="images/404.png"><br></div>
-	  	    <input type="button" value="jouer" id="boutonjouer" onclick="lancer = play(lancer);"><br>
-	  	    <div id="jeu">
-	  	    	<input type="button" id="afficherpoint" value="point : 0"> 
-	  	    	<img alt="ennemie" src="images/ennemie.gif" id="ennemie">
-	  	    	<img alt="piece1" src="images/piece.png" id="piece">  <br><br>
-	  	    	<img alt="personnage" src="images/perso.gif" id="perso"> 
-	  	    	
-	            
 	  	    </div>
 	  </body>
 </html>	  
