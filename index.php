71b9c481209649a3f2135f3a12b45da5fcb29c96
victordalet <victordalet@protonmail.com>
2021-09-15 22:16:04 +0200

    page de connexion

--- index.php
+++ index.php
@@ -6,12 +6,13 @@
 		<link rel="stylesheet" type="text/css" href="css/style_image.css"> <!-- on imparte le css -->
 		<link rel="stylesheet" type="text/css" href="css/style_police.css"> <!-- on imparte le css -->
 		<link rel="stylesheet" type="text/css" href="css/style_bouton.css"> <!-- on imparte le css -->
+		<script src="js/redirection.js"></script> <!-- on import le js -->
 	      <link rel="icon" type="image/png" href="images/logo.png" /> <!-- logo à coté du titre-->
 	</head>
 	        
 	<body>
-		<div onclick="redirect(0,1);" class="btn from-left">Virginie</div> 
 		<div onclick="redirect(1,1);" class="btn from-left">Collage</div>
+		<div onclick="redirect(0,1);" class="btn from-left">Virginie</div> 
 		<div onclick="redirect(2,1);" class="btn from-left">Peinture</div>
 		<div onclick="redirect(3,1);" class="btn from-left">Contact</div> 				
 		<div id="contenu_pres">
