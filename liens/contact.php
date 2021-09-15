db96cca6522a94ff39f82ea7039e98600e006f8e
victordalet <victordalet@protonmail.com>
2021-09-15 22:36:37 +0200

    page de contact

--- liens/contact.php
+++ liens/contact.php
@@ -1,41 +1,50 @@
 <!doctype HTML>
 <html lang="fr">
       <head> 
-	      <title> CONTACT VIRGINIE DUHAMEL  </title> <!--le titre de la page -->
-		<link rel="stylesheet" type="text/css" href="../css/style_div.css"> <!-- on imparte le css -->
-		<link rel="stylesheet" type="text/css" href="../css/style_image.css"> <!-- on imparte le css -->
-		<link rel="stylesheet" type="text/css" href="../css/style_police.css"> <!-- on imparte le css -->
-		<link rel="stylesheet" type="text/css" href="../css/style_bouton.css"> <!-- on imparte le css -->
+	      <title> CONTACT VIRGINIE DUHAMEL  </title> <!--le titre de la page -->>
+		<link rel="stylesheet" type="text/css" href="../css/style_contact.css"> <!-- on imparte le css -->
 		<script src="../js/galerie.js"></script> <!-- on import le js -->
 		<script src="../js/redirection.js"></script> <!-- on import le js -->
 	      <link rel="icon" type="image/png" href="../images/logo.png" /> <!-- logo à coté du titre-->
 	</head>
 	        
 	<body>
-		<div onclick="redirect(0,2);" class="btn from-left">Virginie</div> 
-		<div onclick="redirect(1,2);" class="btn from-left">Collage</div>
-		<div onclick="redirect(2,2);" class="btn from-left">Peinture</div>
-		<div onclick="redirect(3,2);" class="btn from-left">Contact</div> 				
-		<div id="formulaire">
-			<form method = "POST" id="envoie" action = "envoie.php">
-			<h1>Fancy Text Inputs</h1>
-				  <div class="question">
-				    <input type="text" required/>
-				    <label>Nom (requis)</label>
-				  </div>
-				  <div class="question">
-				    <input type="text" required/>
-				    <label>Email (requis)</label>
-				  </div>
-				  <div class="question">
-				    <input type="text" required/>
-				    <label>Sujet</label>
-				  </div>
-				  <div class="question">
-				    <input type="text" required/>
-				    <label>Message</label>
-				  </div>
-				  <button>Envoie</button>
+		<form method = "POST" id="envoie" action = "envoie.php">
+				<div class="container">
+					<div class="row">
+							<h1>contacter nous</h1>
+					</div>
+					<div class="row input-container">
+							<div class="col-xs-12">
+								<div class="styled-input wide">
+									<input type="text" name="name" required />
+									<label>Nom (Requis)</label> 
+								</div>
+							</div>
+							<div class="col-md-6 col-sm-12">
+								<div class="styled-input">
+									<input type="text" name="email" required />
+									<label>Email (Requis)</label> 
+								</div>
+							</div>
+							<div class="col-md-6 col-sm-12">
+								<div class="styled-input" style="float:right;">
+									<input type="text" name="sujet"  />
+									<label>Sujet</label> 
+								</div>
+							</div>
+							<div class="col-xs-12">
+								<div class="styled-input wide">
+									<textarea name="message" required></textarea>
+									<label>Message</label>
+								</div>
+							</div>
+							<div class="col-xs-12">
+								<div class="btn-lrg submit-btn">Envoie</div>
+							</div>
+							<br><br>
+					</div>
+				</div>
 			</form>	 
 		</div >
       </body>
