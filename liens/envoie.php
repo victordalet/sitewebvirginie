9fefb280a4a720bfd1799975c5c43e1362e01579
victordalet <victordalet@protonmail.com>
2021-09-28 21:44:42 +0200

    envoie de mail via le formulaire de contacte

--- liens/envoie.php
+++ liens/envoie.php
@@ -1,7 +1,19 @@
 <?php
-$date = date("(d.m.y ; H:i) : "); //la date de l'envoie du message
-$document = fopen("messagerie.json","c+"); // on ouvre le fichier 
-fseek($document, filesize('messagerie.json'));
-fwrite($document, '<span class="nom">'.$_POST["name"].'</span>'.'<span class="date">'.$date.'</span>'.'<span class="email">'.$_POST["email"].'</span>'.'<span class="sujet">'.$_POST["sujet"].'</span>'.'<span class="message">'.$_POST["message"].'</span>'.'<br>'."\n" ); 
-header('Location: contact.php' );
+
+	$date = date("(d.m.y ; H:i) : ");
+	$document = fopen("messagerie.json","c+"); 
+	fseek($document, filesize('messagerie.json'));
+	fwrite($document, '<span class="nom">'.$_POST["name"].'</span>'.'<span class="date">'.$date.'</span>'.'<span class="email">'.$_POST["email"].'</span>'.'<span class="sujet">'.$_POST["sujet"].'</span>'.'<span class="message">'.$_POST["message"].'</span>'.'<br>'."\n" ); 
+
+
+	$to      = 'virginiesophie.duhamel@orange.fr';
+	$subject = $_POST['sujet'];
+	$message = $_POST['message'];
+	$headers = 'From:'.$_POST['email'] . "\r\n" .
+	'Reply-To: webmaster@example.com' . "\r\n" .
+	'X-Mailer: PHP/' . phpversion();
+
+	mail($to, $subject, $message, $headers);
+	header('Location: contact.php' );
+
 ?>
